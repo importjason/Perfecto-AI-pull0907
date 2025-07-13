9cdabbffb4c1bf32368bcb64e4ee9c1d25de85f3
Lova-clover <woeg0226@naver.com>
2025-07-13 21:06:41 +0900

    whiser에서aeneas로대체

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from rag_pipeline import get_retriever_from_source, get_document_chain, get_defa
 from web_ingest import full_web_ingest # web_ingest는 별도로 정의되어 있어야 합니다.
 from image_generator import generate_images_for_topic
 from elevenlabs_tts import generate_tts, TTS_TEMPLATES
-from aeneas import transcribe_audio_with_timestamps, generate_ass_subtitle, SUBTITLE_TEMPLATES 
+from aeneas_asr import transcribe_audio_with_timestamps, generate_ass_subtitle, SUBTITLE_TEMPLATES 
 from video_maker import create_video_with_segments, add_subtitles_to_video
 from deep_translator import GoogleTranslator
 import os
